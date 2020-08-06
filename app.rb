41c413a800577d0b48a314417b55c111f619066e
TJBachorz <TJBachorz@gmail.com>
2020-08-06 15:19:04 -0600

    completed class assignment

--- app.rb
+++ app.rb
@@ -1,38 +1,54 @@
 require 'pry'
 
 class Building
+    #The name and number of tenants should be readable and writeable
     attr_accessor :name, :tenants
+    #The address should be readable
     attr_reader :address
+
+    #The number of floors should not be readable or writeable
 
     @@all = []
 
+#Initializes with a name, address, number of floors, and number of tenants
     def initialize(name, address, floors, tenants)
         @name = name
         @address = address
         @floors = floors
         @tenants = tenants
-        @@all << self
+        @@all << self # The class should keep track of all buildings that are created
     end
 
     def self.all
         @@all
     end
 
-    def self.tenants_per_building(building_name)
-        all.find {|building| building.name == building_name}.tenants
+    #The class should tell the average number of tenants in a building
+    
+    def self.tenants_per_building
+        all.map {|building| building.tenants }
     end
 
     def self.average_tenants_of_buildings
-        tenants_list = all.map {|building| building.tenants}
+        tenants_list = self.tenants_per_building
         tenants_list.reduce(:+) / tenants_list.length
-        end
     end
 
-    def self.placard
-        @name, @address
+    #A building should have a placard method that returns the name and address of the building
+
+    def placard
+        "#{@name}, #{@address}"
+    end
+
+    #A building should have a method returns the average number of tenants per floor
+
+    def average_tenants_per_floor
+        (@tenants.to_f / @floors.to_f).round(2)
     end
 
 end
+
+#adding buildings as variables and to the class
 
 sears_tower = Building.new("Sear's Tower", "Chicago, IL", 110, 1560)
 empire_state = Building.new("Empire State Building", "New York, New York", 102, 1200) 
