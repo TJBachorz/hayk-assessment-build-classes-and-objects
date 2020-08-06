0f552a02e483d8063d89fbe7ffb01a4184280dda
TJBachorz <TJBachorz@gmail.com>
2020-08-06 14:10:36 -0600

    adds .self functionality

--- app.rb
+++ app.rb
@@ -1,43 +1,45 @@
 require 'pry'
 
-Class Building
-    attr_accessor :name, :address, :floors, :tenants
+class Building
+    attr_accessor :name, :tenants
+    attr_reader :address
+
+    @@all = []
 
     def initialize(name, address, floors, tenants)
         @name = name
         @address = address
         @floors = floors
         @tenants = tenants
-    end
-    
-    def name=(building_name)
-        @name = building_name
+        @@all << self
     end
 
-    def name
-        @name
+    def self.all
+        @@all
     end
 
-    def address
-        @address
+    def self.tenants_per_building(building_name)
+        all.find {|building| building.name == building_name}.tenants
     end
 
-    def tenants=(number_of_tenants)
-        @tenants = number_of_tenants
+    def self.average_tenants_of_buildings
+        tenants_list = all.map {|building| building.tenants}
+        tenants_list.reduce(:+) / tenants_list.length
+        end
     end
 
-    def tenants
-        @tenants
+    def self.name_and_address
+        
     end
-
-    buildings = [
-        Building.new("Sear's Tower", "Chicago, IL", 
-    ]
-
-    def average_tenants
-    
-
-
-
 
 end
+
+Building.new("Sear's Tower", "Chicago, IL", 110, 1560)
+Building.new("Empire State Building", "New York, New York", 102, 1200) 
+Building.new("White House", "Washington, DC", 4, 4)
+Building.new("Big Ben", "London, England", 50, 250)
+
+
+
+binding.pry
+'hi'
